c1c47170ac0ba29b19b7b9224056af46048dbc9c
Matthew January <matthewjanuary@gmail.com>
2023-05-11 11:13:37 +0100

    Adding the webpage to git.

---
+++ var/www/html/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background-color: lightblue;
+}
+</style>
+</head>
+<body>
+
+<h1>Hello World!</h1>
+
+<?php
+
+echo "This page has a light blue background color!"
+
+?>
+
+</body>
+</html>
