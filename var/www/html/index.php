e6a7c57a056366c2a66f792865926a4c3b75bb61
Matthew January <matthewjanuary@gmail.com>
2023-05-11 13:41:26 +0100

    Created subdirectory for data so that index.php can access the data easily. Need to create a python script that will migrate the files once they are ready for viewing to the correct directories.

--- var/www/html/index.php
+++ var/www/html/index.php
@@ -1,21 +1,42 @@
 <!DOCTYPE html>
 <html>
-<head>
-<style>
-body {
-  background-color: lightblue;
-}
-</style>
-</head>
-<body>
+    <head>
+	<style>
+	    body {
+  		background-color: lightblue;
+	    }
+	</style>
+    </head>
+    <body>
+	<center>
+	<h1>Hello World!</h1>
 
-<h1>Hello World!</h1>
+	<?php
+	//$file_path = "~/EEE4113F/*.csv";
 
-<?php
+	echo "<html><body><table>\n\n";
+  	//echo $data_path;
+        // Open a file
+        $file = fopen("SampleCSVFile_2kb.csv", "r");
+  	//echo $file;
+        // Fetching data from csv file row by row
+        while (($data = fgetcsv($file)) !== false) {
+  
+            // HTML tag for placing in row format
+            echo "<tr>";
+            foreach ($data as $i) {
+                echo "<td>" . htmlspecialchars($i) . "</td>";
+            }
+            echo "</tr> \n";
+        }
+  
+        // Closing the file
+        fclose($file);
+  
+        echo "\n</table></body></html>";
 
-echo "This page has a light blue background color!"
-
-?>
-
-</body>
+	?>
+	</center>
+    </body>
 </html>
+
